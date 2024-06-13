b5be16679e07b26347409bd6e7019b5208585a65
Vishalnag V <vishalnag.vittalaraya@ordwaylabs.com>
2024-06-13 11:05:32 +0530

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ marginTop: "75px" }}>
+      <div id="ordway_ssp"></div>
+    </div>
+  );
+};
+
+export default Home;
